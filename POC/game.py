68c7e067736c3be0848551545f61bdb33af4b314
kojalowicz <piotr.kojalowicz@gmail.com>
2024-04-05 10:49:24 +0200

    Init TTT Game

---
+++ POC/game.py
@@ -0,0 +1,7 @@
+class TicTacToe:
+    def __init__(self, num_players, player_names, grid_size):
+        self.num_players = num_players
+        self.player_names = player_names
+        self.grid_size = grid_size
+
+    # Implement game logic methods here
